b4e09f9319c6088850f0818d9ece03401ee7d8a0
Rediat <yaidercc@gmail.com>
2022-08-01 13:16:50 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const GifExpertApp=()=>{
+    const [categories,setCategories]=useState(['one punch']);
+
+    return(
+        <>
+            <h1>GifExpertApp</h1>
+        </>
+    );
+} 
